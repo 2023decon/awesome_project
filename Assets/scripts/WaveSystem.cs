57677f164640f76ec3cbae99ff309317102fb97e
h0stuck <taeroesboy@naver.com>
2023-11-05 22:00:54 +0900

    1105 wave

--- Assets/scripts/WaveSystem.cs
+++ Assets/scripts/WaveSystem.cs
@@ -6,11 +6,12 @@ using UnityEngine;
 public struct EnemyWave
 {
     public int type;
-    public int pos;
+    public float posX;
+    public float posY;
     public float time;
-    public EnemyWave(int type, int pos, float time)
+    public EnemyWave(int type, float posX, float posY, float time)
     {
-        this.type = type; this.pos = pos; this.time = time;
+        this.type = type; this.posX = posX; this.posY = posY; this.time = time;
     }
 }
 
@@ -18,16 +19,27 @@ public class WaveSystem : MonoBehaviour
 {
     public List<GameObject> enemyPrefabs;
     public List<EnemyWave> wave;
+    private float timer = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        wave.Add(new EnemyWave(0, 0, 0, 1));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if(timer >= wave[0].time)
+        {
+            Spawn(wave[0]);
+            wave.Remove(wave[0]);
+        }
+    }
+
+    private void Spawn(EnemyWave ew)
+    {
+        Instantiate(enemyPrefabs[ew.type]);
     }
 }
