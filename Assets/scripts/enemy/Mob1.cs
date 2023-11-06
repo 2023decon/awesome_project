e47d040ff1e39e2f34ee8051c120ed71f336c8be
h0stuck <taeroesboy@naver.com>
2023-11-06 11:00:32 +0900

    1106 attack fix

--- Assets/scripts/enemy/Mob1.cs
+++ Assets/scripts/enemy/Mob1.cs
@@ -15,15 +15,19 @@ public class Mob1 : Enemy
         if (!isOutRange && status == StatusEffect.DEFAULT) {
             atkDelay_ += Time.deltaTime;
 
-            if (atkDelay_ > atkDelay) {
+            if (atkDelay_ > atkDelay)
+            {
+                if (player.transform.position.x < transform.position.x)
+                    spriteRenderer.flipX = true;
+                else if (player.transform.position.x > transform.position.x)
+                    spriteRenderer.flipX = false;
+
                 atkDelay_ = 0;
 
                 animator.ResetTrigger("attack");
                 animator.SetTrigger("attack");
 
-                if (Vector2.Distance(player.transform.position, transform.position) <= attackRange) {
-                    player.Hurt(attackDamage);
-                }
+                StartCoroutine(HitJudgement());
             }
         }
     }
@@ -33,5 +37,16 @@ public class Mob1 : Enemy
         animator.ResetTrigger("hurt");
         animator.SetTrigger("hurt");
 
+    }
+
+    private IEnumerator HitJudgement()
+    { 
+        yield return new WaitForSeconds(0.25f);
+        if (Vector2.Distance(player.transform.position, transform.position) <= attackRange &&
+            ((player.transform.position.x > transform.position.x && !spriteRenderer.flipX) ||
+            (player.transform.position.x < transform.position.x && spriteRenderer.flipX)))
+        {
+            player.Hurt(attackDamage);
+        }
     }
 }
